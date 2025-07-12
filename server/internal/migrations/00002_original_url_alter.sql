9e707e2ffb1669a998f0cc32f218a65839e140bf
atharvadani9 <atharvadani9@gmail.com>
2025-07-12 18:58:51 +0530

    alter original_url column

--- server/internal/migrations/00002_original_url_alter.sql
+++ server/internal/migrations/00002_original_url_alter.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE tinyurl 
-ALTER COLUMN original_url VARCHAR(1024);
+ALTER TABLE tinyurl
+ALTER COLUMN original_url TYPE VARCHAR(1024);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE tinyurl 
-ALTER COLUMN original_url VARCHAR(255);
+ALTER TABLE tinyurl
+ALTER COLUMN original_url TYPE VARCHAR(255);
 -- +goose StatementEnd
