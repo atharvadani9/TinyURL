38c46cc3ed03fb701d889f06700f3ea8f35f55d9
atharvadani9 <atharvadani9@gmail.com>
2025-07-12 16:10:50 +0530

    added get api

--- server/internal/api/tinyurl_handler.go
+++ server/internal/api/tinyurl_handler.go
@@ -13,6 +13,10 @@ type createTinyURLRequest struct {
 	OriginalURL string `json:"originalUrl"`
 }
 
+type getTinyURLRequest struct {
+	ShortURL string `json:"shortUrl"`
+}
+
 type TinyURLHandler struct {
 	tinyURLStore store.TinyURLStore
 	logger       *log.Logger
@@ -41,7 +45,7 @@ func (h *TinyURLHandler) CreateTinyURL(w http.ResponseWriter, r *http.Request) {
 	existingTinyURL, err := h.tinyURLStore.GetByOriginalURL(req.OriginalURL)
 	if err == nil && existingTinyURL != nil {
 		h.logger.Printf("Returning existing tiny URL: %v", existingTinyURL.ShortURL)
-		utils.WriteJSON(w, http.StatusOK, utils.Envelope{"tinyUrl": existingTinyURL.ShortURL})
+		utils.WriteJSON(w, http.StatusOK, utils.Envelope{"shortUrl": existingTinyURL.ShortURL})
 		return
 	}
 
@@ -67,7 +71,7 @@ func (h *TinyURLHandler) CreateTinyURL(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.logger.Printf("Tiny URL created: %v", tinyURL.ShortURL)
-	utils.WriteJSON(w, http.StatusCreated, utils.Envelope{"tinyUrl": tinyURL.ShortURL})
+	utils.WriteJSON(w, http.StatusCreated, utils.Envelope{"shortUrl": tinyURL.ShortURL})
 }
 
 func generateShortURL() string {
@@ -75,5 +79,21 @@ func generateShortURL() string {
 }
 
 func (h *TinyURLHandler) GetTinyURL(w http.ResponseWriter, r *http.Request) {
+	var req getTinyURLRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		h.logger.Printf("Error decoding request body: %v", err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "Invalid request body"})
+		return
+	}
 
+	tinyURL, err := h.tinyURLStore.Get(req.ShortURL)
+	if err != nil {
+		h.logger.Printf("Error getting tiny URL: %v", err)
+		utils.WriteJSON(w, http.StatusNotFound, utils.Envelope{"error": "Tiny URL not found"})
+		return
+	}
+
+	h.logger.Printf("URL found: %v", tinyURL.OriginalURL)
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{"originalUrl": tinyURL.OriginalURL})
 }
